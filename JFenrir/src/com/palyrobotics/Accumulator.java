5ee96422bf63c52a8243e413632e08439c5fcf26
Nihar Mitra <nihar.roy.mitra@gmail.com>
2014-06-05 22:05:43 -0700

    Fixed indentation for switch statement, removed duplicate method.

--- JFenrir/src/com/palyrobotics/Accumulator.java
+++ JFenrir/src/com/palyrobotics/Accumulator.java
@@ -13,67 +13,67 @@ import edu.wpi.first.wpilibj.*;
  * @author Neelay Junnarkar
  */
 public class Accumulator extends Subsystem {
-	
+
 	private Victor accuVic;
-	
+
 	private int state;
 	public void setState(int state){ this.state = state; }
 	public int getState(){ return state; }
-	
+
 	private static final int IDLE = 0, ACCUMULATING = 1, PASSING = 2; 
-	
+
 	public Accumulator() {
 		accuVic = new Victor(Constants.PORT_ACCUMULATOR_VIC_7);
 	}
-	
+
 	public void init() {
 		state = IDLE;
 	}
-	
+
 	public void disable() {
 		state = IDLE;
 	}
-	
+
 	public void update() {
 		switch(state) {
-			case IDLE:
-				setAllVics(0.0);
-				break;
-			case ACCUMULATING:
-				accuVic.set(-1.0);
-				break;
-			case PASSING:
-				accuVic.set(1.0);
-				break;
+		case IDLE:
+			setAllVics(0.0);
+			break;
+		case ACCUMULATING:
+			accuVic.set(-1.0);
+			break;
+		case PASSING:
+			accuVic.set(1.0);
+			break;
 		}
 	}
-    
+
 	private void setAllVics(double spd) {
 		accuVic.set(spd);
 	}
-	
+
 	public static class AccumulateCommand extends RobotCommand {
-		
+
 		public AccumulateCommand() {
 			subsystemType = RobotCommand.ACCUMULATOR;
 		}
-		
+
 		void execute(Accumulator accu) {
 			accu.setState(ACCUMULATING);
 		}
 	}
-    
+
 	public static class PassCommand extends RobotCommand {
-		
+
 		public PassCommand() {
 			subsystemType = RobotCommand.ACCUMULATOR;
 		}
-		
+
 		void execute(Accumulator accu){
 			accu.setState(PASSING);
 		}
-    }
-    
+	}
+
 	public static class FlushCommand extends RobotCommand {
 		public FlushCommand() {
 			subsystemType  = RobotCommand.ACCUMULATOR;
@@ -83,9 +83,9 @@ public class Accumulator extends Subsystem {
 			accu.setState(PASSING);
 		}
 	}
-	
+
 	public static class SetIdleCommand extends RobotCommand {
-		
+
 		public SetIdleCommand() {
 			subsystemType = RobotCommand.ACCUMULATOR;
 		}
@@ -94,9 +94,9 @@ public class Accumulator extends Subsystem {
 			accu.setState(IDLE);
 		}
 	}
-	
+
 	public static class EjectCommand extends RobotCommand {
-		
+
 		public EjectCommand() {
 			subsystemType = RobotCommand.ACCUMULATOR;
 		}
